571a07bf06aed739ef83e48cf50127ce72ae21dc
midnight-trigger <junya4318@gmail.com>
2022-11-27 11:05:40 +0900

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({ credentials: true, origin: ['http://localhost:3000'] });
+  app.enableCors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'https://todo-app-client-next.vercel.app',
+    ],
+  });
   app.use(cookieParser());
   app.use(
     csurf({
